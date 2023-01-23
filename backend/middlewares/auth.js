c27e6892952f1cc3aacc621879c89357620c0802
Three Worlds <harrymidas@yandex.ru>
2023-01-23 17:39:19 +0300

    Изменяет логику аутентификации

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,18 +7,16 @@ const auth = async (req, res, next) => {
   const { authorization } = await req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new Unauthorized('Необходима авторизация'));
+  } else {
+    const token = authorization.replace('Bearer ', '');
+    let payload;
+    try {
+      payload = jwt.verify(token, 'JWT_SECRET');
+    } catch (err) {
+      next(new Unauthorized('Необходима авторизация'));
+    }
+    req.user = payload;
   }
-
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-  try {
-    payload = jwt.verify(token, 'JWT_SECRET');
-  } catch (err) {
-    next(new Unauthorized('Необходима авторизация'));
-  }
-
-  req.user = payload;
-
   next();
 };
 
